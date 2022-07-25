511d78da90a5db15e1ec3dcbbbd7dc54ea557f94
Anıl İnce <100514281+anilince@users.noreply.github.com>
2022-07-25 12:56:17 +0300

    Add files via upload

---
+++ 23.07.2022/9-Js/project.js
@@ -0,0 +1,40 @@
+// console.log("Merhaba");
+// console.log('Merhaba');
+
+// var
+// let
+// const
+
+let gun = 12;
+let ay = 9;
+let yil = 1997;
+
+gun = 15;
+
+console.log(gun,ay,yil);
+console.log("Ahmet Kerem Kantarcı "+gun+"/"+ay+"/"+yil+" tarihinde doğmuştur");
+
+const ikincielAraba = 200000;
+// ikincielAraba = 130000;
+console.log(ikincielAraba);
+
+var a = 5;
+var a = 3;
+console.log(a);
+
+var c = 5;
+console.log("C değeri dışarıda",c);
+{
+    var c = 3;
+    console.log("C değeri içeride",c);
+}
+
+console.log("C değeri dışarıda",c);
+
+let d = 5;
+console.log("D değeri dışarıda",d);
+{
+    let d = 3;
+    console.log("D değeri içeride",d);
+}
+console.log("D değeri dışarıda",d);
